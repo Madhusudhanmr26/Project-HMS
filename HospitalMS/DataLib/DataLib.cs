16212eef86d379db28512353d32ec2f2245f1ca6
Madhu sudhan M R <64772957+Madhusudhanmr26@users.noreply.github.com>
2020-05-05 18:32:47 +0530

    First stage

---
+++ HospitalMS/DataLib/DataLib.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DataLib;
+
+namespace HospitalDal
+{
+    public interface IDataComponent
+    {
+        void AddInPatient(InPatient patient);
+        void UpdateInPatient(InPatient patient);
+        List<InPatient> GetAllInPatient();
+        void AddOutPatient(OutPatient patient);
+        void UpdateOutPatient(OutPatient patient);
+        void DeleteOutPatient(int OutPatientID);
+        void AddStaff(Staff staff);
+        void UpdateStaff(Staff staff);
+        void DeleteStaff(int ID);
+        List<Staff> GetAllStaff();
+        Staff LoginStaff(string name);
+        void RegisterStaff(Staff staff);
+        void AddBill(Bill bill);
+        void UpdateBill(Bill bill);
+    }
+
+    public class DataComponent : IDataComponent
+    {
+        static HospitalAppEntities1 hos = new HospitalAppEntities1();
+        public void AddBill(Bill bill)
+        {
+            hos.Bills.Add(bill);
+            hos.SaveChanges();
+        }
+
+        public void AddInPatient(InPatient patient)
+        {
+
+            hos.InPatients.Add(patient);
+            hos.SaveChanges();
+        }
+
+        public void AddOutPatient(OutPatient patient)
+        {
+            hos.OutPatients.Add(patient);
+            hos.SaveChanges();
+        }
+
+        public void AddStaff(Staff staff)
+        {
+            hos.Staffs.Add(staff);
+            hos.SaveChanges();
+        }
+
+        public void DeleteOutPatient(int OutPatientID)
+        {
+            try
+            {
+                var selected = hos.OutPatients.FirstOrDefault((a) => a.OutPatientID == OutPatientID);
+                if (selected == null) throw new Exception("OutPatient not found to delete");
+                hos.OutPatients.Remove(selected);
+                hos.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void DeleteStaff(int ID)
+        {
+            throw new NotImplementedException();
+        }
+        public List<InPatient> GetAllInPatient()
+        {
+            return new HospitalAppEntities1().InPatients.ToList();
+        }
+
+        public List<Staff> GetAllStaff()
+        {
+            return new HospitalAppEntities1().Staffs.ToList();
+        }
+
+        public Staff LoginStaff(string name)
+        {
+            var selected = hos.Staffs.FirstOrDefault((u) => u.Name ==name);
+            if (selected == null) throw new Exception("Login failed for the user");
+            return selected;
+        }
+
+        public void RegisterStaff(Staff staff)
+        {
+            hos.Staffs.Add(staff);
+            hos.SaveChanges();
+        }
+
+        public void UpdateBill(Bill bill)
+        {
+            var selected = hos.Bills.FirstOrDefault((e) => e.InPatientID ==bill.InPatientID);
+            if (selected == null) throw new Exception("Patient not found to update");
+            hos.SaveChanges();
+        }
+
+        public void UpdateInPatient(InPatient patient)
+        {
+            var selected = hos.InPatients.FirstOrDefault((e) => e.InPatientID == patient.InPatientID);
+            if (selected == null) throw new Exception("Patient not found to update");
+            hos.SaveChanges();
+        }
+
+        public void UpdateOutPatient(OutPatient patient)
+        {
+            var selected = hos.OutPatients.FirstOrDefault((e) => e.OutPatientID == patient.OutPatientID);
+            if (selected == null) throw new Exception("Patient not found to update");
+            hos.SaveChanges();
+        }
+
+        public void UpdateStaff(Staff staff)
+        {
+            var selected = hos.Staffs.FirstOrDefault((e) => e.ID == staff.ID);
+            if (selected == null) throw new Exception("Staff not found to update");
+            hos.SaveChanges();
+        }
+        public static class DataFactory
+        {
+            public static IDataComponent CreateComponent()
+            {
+                return new DataComponent();
+            }
+
+        }
+    }
+}
+    
+
---
+++ HospitalMS/HospitalBall/DataBall.cs
@@ -0,0 +1,102 @@
+﻿using DataLib;
+using HospitalDal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HospitalDal.DataComponent;
+
+namespace HospitalBall
+{
+    public interface IHospitalComponent
+    {
+        void AddInPatient(InPatient patient);
+        void UpdateInPatient(InPatient patient);
+        List<InPatient> GetAllInPatient();
+        void AddOutPatient(OutPatient patient);
+        void UpdateOutPatient(OutPatient patient);
+        void DeleteOutPatient(int OutPatientID);
+        void AddStaff(Staff staff);
+        void UpdateStaff(Staff staff);
+        void DeleteStaff(int ID);
+        List<Staff> GetAllStaff();
+        Staff LoginStaff(string name);
+        void RegisterStaff(Staff staff);
+        void AddBill(Bill bill);
+        void UpdateBill(Bill bill);
+    }
+    public class HospitalComponent : IHospitalComponent
+    {
+        static IDataComponent pat = DataFactory.CreateComponent();
+        public void AddBill(Bill bill)
+        {
+            pat.AddBill(bill);
+        }
+        public void AddInPatient(InPatient patient)
+        {
+            pat.AddInPatient(patient);
+        }
+
+        public void AddOutPatient(OutPatient patient)
+        {
+            pat.AddOutPatient(patient);
+        }
+
+        public void AddStaff(Staff staff)
+        {
+            pat.AddStaff(staff);
+        }
+
+        public void DeleteOutPatient(int OutPatientID)
+        {
+            pat.DeleteOutPatient(OutPatientID);
+        }
+
+        public void DeleteStaff(int ID)
+        {
+            pat.DeleteStaff(ID);
+
+        }
+
+        public List<InPatient> GetAllInPatient()
+        {
+            return pat.GetAllInPatient();
+        }
+
+        public List<Staff> GetAllStaff()
+        {
+            return pat.GetAllStaff();
+        }
+
+        public Staff LoginStaff(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterStaff(Staff staff)
+        {
+            pat.RegisterStaff(staff);
+        }
+
+        public void UpdateBill(Bill bill)
+        {
+            pat.UpdateBill(bill);
+        }
+
+        public void UpdateInPatient(InPatient patient)
+        {
+            pat.UpdateInPatient(patient);
+        }
+
+        public void UpdateOutPatient(OutPatient patient)
+        {
+            pat.UpdateOutPatient(patient);
+        }
+
+        public void UpdateStaff(Staff staff)
+        {
+            pat.UpdateStaff(staff);
+        }
+    }
+}
